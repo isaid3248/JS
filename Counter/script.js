0548e95d25455ad394df9ee337cc3e56f289c6e5
isaid3248 <isaid3248@gmail.com>
2021-07-05 22:39:24 +0300

    JS

---
+++ Counter/script.js
@@ -0,0 +1,20 @@
+const screenEL = document.querySelector(".screen");
+const positiveButton = document.querySelector(".positive");
+const negativeButton = document.querySelector(".negative");
+
+console.log(screenEL, positiveButton, negativeButton);
+
+let button = 0;
+
+positiveButton.addEventListener("click",() => {
+    button = button + 1;
+    console.log("Positive is clicked", button);
+    screenEL.innerHTML = button;
+});
+
+negativeButton.addEventListener("click",() => {
+    button = button - 1;
+    console.log("Negative is clicked", button);
+    screenEL.innerHTML = button;
+});
+
