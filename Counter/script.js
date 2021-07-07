8fa2f329c35dfe3f7453f0833fdd95979d25a09e
isaid3248 <isaid3248@gmail.com>
2021-07-07 22:18:16 +0300

    JS

--- Counter/script.js
+++ Counter/script.js
@@ -6,15 +6,14 @@ console.log(screenEL, positiveButton, negativeButton);
 
 let button = 0;
 
-positiveButton.addEventListener("click",() => {
-    button = button + 1;
-    console.log("Positive is clicked", button);
-    screenEL.innerHTML = button;
+positiveButton.addEventListener("click", () => {
+  button = button + 1;
+  console.log("Positive is clicked", button);
+  screenEL.innerHTML = button;
 });
 
-negativeButton.addEventListener("click",() => {
-    button = button - 1;
-    console.log("Negative is clicked", button);
-    screenEL.innerHTML = button;
+negativeButton.addEventListener("click", () => {
+  button = button - 1;
+  console.log("Negative is clicked", button);
+  screenEL.innerHTML = button;
 });
-
